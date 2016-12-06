d62036b242f45f83e6427e698a014208ee5e0d1a
houyi1121 <houyi1121@163.com>
2016-12-06 09:45:53 +0800

    备份

--- scripts/node.js
+++ scripts/node.js
@@ -9,33 +9,33 @@ _exec('start "" "D:\\Program Files (x86)\\Microsoft VS Code\\Code.exe" ' + data.
 
 //监听 当deploy完成后执行备份
 
-// require('shelljs/global');
-// try {
-// 	hexo.on('deployAfter', function() {//当deploy完成后执行备份
-// 		run();
-// 	});
-// } catch (e) {
-// 	console.log("产生了一个错误<(￣3￣)> !，错误详情为：" + e.toString());
-// }
-// function run() {
-// 	if (!which('git')) {
-// 		echo('Sorry, this script requires git');
-// 		exit(1);
-// 	} else {
-// 		echo("======================Auto Backup Begin===========================");
-// 		cd('C:/Users/houyi/blog');    //此处修改为Hexo根目录路径
-// 		if (exec('git add --all').code !== 0) {
-// 			echo('Error: Git add failed');
-// 			exit(1);
-// 		}
-// 		if (exec('git commit -am "Form auto backup script\'s commit"').code !== 0) {
-// 			echo('Error: Git commit failed');
-// 			exit(1);
-// 		}
-// 		if (exec('git push blog hexo').code !== 0) {
-// 			echo('Error: Git push failed');
-// 			exit(1);
-// 		}
-// 		echo("==================Auto Backup Complete============================")
-// 	}
-// }
+require('shelljs/global');
+try {
+	hexo.on('deployAfter', function() {//当deploy完成后执行备份
+		run();
+	});
+} catch (e) {
+	console.log("产生了一个错误<(￣3￣)> !，错误详情为：" + e.toString());
+}
+function run() {
+	if (!which('git')) {
+		echo('Sorry, this script requires git');
+		exit(1);
+	} else {
+		echo("======================Auto Backup Begin===========================");
+		cd('C:/Users/houyi/blog');    //此处修改为Hexo根目录路径
+		if (exec('git add --all').code !== 0) {
+			echo('Error: Git add failed');
+			exit(1);
+		}
+		if (exec('git commit -am "Form auto backup script\'s commit"').code !== 0) {
+			echo('Error: Git commit failed');
+			exit(1);
+		}
+		if (exec('git push blog hexo').code !== 0) {
+			echo('Error: Git push failed');
+			exit(1);
+		}
+		echo("==================Auto Backup Complete============================")
+	}
+}
