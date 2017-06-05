3679fc1746cca570454bc91a38febdc276b9cb4c
ihoey <mail@ihoey.com>
2017-06-05 17:57:52 +0800

    这个勤奋的家伙又更新源文件

--- server.sh
+++ server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 scp -r public/ ihoey:scp/
-ssh ihoey >> ihoey 
-cd scp/ && sudo mv public/ /var/www/ && sudo rm -rf /var/www/hexo && sudo mv /var/www/public/ /var/www/hexo 
-ihoey
-echo done!
+ssh ihoey "cd scp/ && sudo mv public/ /var/www/ && sudo rm -rf /var/www/hexo && sudo mv /var/www/public/ /var/www/hexo && exit"
+
